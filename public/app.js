8f8cdb0da37cad186ec00bf520d137f2cd5afa51
nikhilpn725 <nikhilpn725@gmail.com>
2023-01-17 19:34:20 +0530

    message send and change room fun implemented

--- public/app.js
+++ public/app.js
@@ -8,27 +8,63 @@ let roomInput = document.getElementById("roomInput");
 let createRoomBtn = document.getElementById("room_addroom_add_icon_holder");
 let chatDisplay = document.getElementById("chat");
 
-
-let currentRoom = "globalChat"
+let currentRoom = "globalChat";
 let myUsername = "";
 
+socket.on("connect", (username) => {
+  myUsername = prompt("Enter name");
+  socket.emit("createUser", myUsername);
+});
 
-socket.on("connect", (username)=>{
-    myUsername = prompt("Enter name")
-    socket.emit("createUser", myUsername)
-})
+socket.on("updateChat", (username, data) => {
+  if (username === "INFO") {
+    chatDisplay.innerHTML += `<div class='announcement'><span>${data}</span></div>`;
+  } else {
+    chatDisplay.innerHTML += `<div > <h2>${data} </h2> </div>`;
+  }
+  chatDisplay.scrollTop = chatDisplay.scrollHeight;
+});
 
-socket.on("updateChat", (username, data)=>{
-    if(username === 'INFO'){
-        chatDisplay.innerHTML +=`<div class='announcement'><span>${data}</span></div>`
-    }else{
-        chatDisplay.innerHTML += `<div > <h2>${data} </h2> </div>`
-    }
-    chatDisplay.scrollTop = chatDisplay.scrollHeight;
-})
+sendMessageBtn.addEventListener("click", () => {
+  socket.emit("message", message.value);
+  message.value = "";
+});
 
-sendMessageBtn.addEventListener("click", ()=>{
-    console.log(message, 'message');
-    socket.emit("message", message.value)
-    message.value = ""
-})
+socket.on("updateUsers", (usernames) => {
+  userList.innerHTML = "";
+  console.log("username returned from server", usernames);
+  for (let user in usernames) {
+    userList.innerHTML += `<div class="user_card">
+        <div class="pic"></div>
+        <span>${user}</span>
+      </div>`;
+  }
+});
+
+socket.on("updateRooms", (rooms, newRoom) => {
+  roomList.innerHTML = "";
+
+  for (var index in rooms) {
+    roomList.innerHTML += `<div class="room_card" id="${rooms[index].name}"
+                                    onclick="changeRoom('${rooms[index].name}')">
+                                    <div class="room_item_content">
+                                        <div class="pic"></div>
+                                        <div class="roomInfo">
+                                        <span class="room_name">#${rooms[index].name}</span>
+                                        <span class="room_author">${rooms[index].creator}</span>
+                                        </div>
+                                    </div>
+                                </div>`;
+  }
+
+  document.getElementById(currentRoom).classList.add("active_item");
+});
+
+function changeRoom(room) {
+  if (room !== currentRoom) {
+    socket.emit("updateRooms", room);
+    document.getElementById(currentRoom).classList.remove("active_item");
+    currentRoom = room;
+    document.getElementById(currentRoom).classList.add("active_item");
+  }
+}
--- server.js
+++ server.js
@@ -11,28 +11,41 @@ const io = new Server(server);
 app.use(express.json());
 app.use(express.static("public"));
 
-const usernames = {}
+const usernames = {};
 const rooms = [
-    {name : "globalChat", creator: "anonymous"},
-    {name : "nofood", creator: "anonymous"},
-    {name : "foodie", creator: "anonymous"},
-]
+  { name: "globalChat", creator: "anonymous" },
+  { name: "nofood", creator: "anonymous" },
+  { name: "foodie", creator: "anonymous" },
+];
 //we need to write all the socket realeated func inside the io.on()
-io.on("connection", (socket)=>{
-    socket.on("createUser", (username)=>{
-        socket.username =username
-        usernames[username] = username
-        socket.currentRoom = "globalChat"
+io.on("connection", (socket) => {
+  socket.on("createUser", (username) => {
+    socket.username = username;
+    usernames[username] = username;
+    socket.currentRoom = "globalChat";
 
-        socket.join("globalChat")
+    socket.join("globalChat");
 
-        socket.emit("updateChat", "INFO", "you have joined Global Chat")
-    })
-    socket.on("message", (data)=>{
-        console.log("msg recieved here",data)
-        io.sockets.to(socket.currentRoom).emit("updateChat", socket.username, data)
-    })
-})
+    socket.emit("updateChat", "INFO", "you have joined Global Chat");
+
+    socket.broadcast.to("globalChat").emit("updateChat", "INFO",  username + " has joined global room")
+
+    io.sockets.emit("updateUsers", usernames);
+    socket.emit("updateRooms", rooms, "globalChat")
+
+  });
+  socket.on("message", (data) => {
+    console.log("msg recieved here", data);
+    io.sockets.to(socket.currentRoom).emit("updateChat", socket.username, data);
+  });
+  socket.on("updateRooms", (data) => {
+        socket.broadcast.to(socket.currentRoom).emit("updateChat", "INFO", socket.username+ "left the room")
+        socket.leave(socket.currentRoom)
+        socket.currentRoom = data
+        socket.join(data)
+        socket.emit("updateChat", "INFO", "You have joined the room", socket.username)
+    });
+});
 
 server.listen(PORT, () => {
   console.log("Server is running at PORT", PORT);
